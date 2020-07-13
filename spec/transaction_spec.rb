51a3a898c15a6284c2f400d8fe1bc616d5841801
nadinedelia <nadine.delia@yahoo.com>
2020-07-13 12:33:32 +0100

    test for initialising trasnaction, realising cash isnt properly formatted

--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -8,6 +8,13 @@ require 'transaction'
 
 
 describe Transaction do
+    it 'is initialised with a date, credit, debit and balance' do
+    date = '14/01/2012'
+    transaction = Transaction.new(date, 1000, 0, 1000)
+    expect(transaction.date).to eq('14/01/2012')
+    expect(transaction.credit).to eq(1000)
+    expect(transaction.debit).to eq(0)
+    expect(transaction.balance).to eq(1000)
     
-
+    end
 end
