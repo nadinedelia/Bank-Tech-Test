d5017039e58a75c27417f0d79183e26ef1ebd390
nadinedelia <nadine.delia@yahoo.com>
2020-07-13 11:18:03 +0100

    money can be deposited and is increasing balance - passing test

--- lib/bank.rb
+++ lib/bank.rb
@@ -3,11 +3,16 @@ class Account
 
     attr_reader :balance
 
+    def initialize
+        @balance = 0
+    end
+
     def deposit(money)
+        @balance += money
     end
 
     def balance
-        0
+        @balance
     end
 
 end
