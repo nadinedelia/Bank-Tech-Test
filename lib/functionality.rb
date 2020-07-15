5770d503844c09c9ecb53a777d08d321bf741e05
nadinedelia <nadine.delia@yahoo.com>
2020-07-15 20:54:10 +0100

    goodbye, functionality class

--- lib/functionality.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-def save_transaction(transaction)
-  @transaction_history << transaction
-end
-
-def raise_error
-  raise 'Transaction not possible - insufficient funds'
-end
