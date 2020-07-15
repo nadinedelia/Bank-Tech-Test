1cf518b086d8b7f23648363e446fa2d07f11992f
nadinedelia <nadine.delia@yahoo.com>
2020-07-15 22:08:18 +0100

    mocking statement class done, 98.7% test coverage

--- spec/statement_spec.rb
+++ spec/statement_spec.rb
@@ -1,7 +1,14 @@
-# frozen_string_literal: true
-
 require 'statement'
 
 describe Statement do
-  # ???????
-end
+
+  let(:statement) { Statement.new }
+  let(:transaction_history) { [transaction] }
+  let(:transaction) { double(date: "15/07/2020", credit: "42.00", debit: "00.00", balance: "42.00") }
+
+    it "displays correct formatting" do
+      formatting = "date || credit || debit || balance\n\n"\
+      "15/07/2020 || 42.00 || 0.00 || 42.00\n"
+      expect{ statement.print(transaction_history) }.to output(formatting).to_stdout
+    end
+  end
