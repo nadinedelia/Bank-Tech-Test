9132637cda54111e41555314de45e06077347757
nadinedelia <nadine.delia@yahoo.com>
2020-07-15 10:04:21 +0100

    decimals displaying

--- lib/account.rb
+++ lib/account.rb
@@ -6,6 +6,7 @@ require_relative 'functionality'
 class Account
   attr_reader :balance, :transaction_history
 
+
   def initialize(transaction = Transaction, statement = Statement.new)
     @balance = 0
     @date = Time.now.strftime('%d/%m/%Y')
@@ -14,25 +15,28 @@ class Account
     @statement = statement
   end
 
+
   def deposit(money)
     if money < 0
         raise_error
     else
     increase_balance(money)
-    credit = new_credit(money)
-    save_transaction(credit)
+    credit_trans = new_credit(money)
+    save_transaction(credit_trans)
     end
   end
+
 
   def withdraw(money)
     if @balance < money || money < 0
       raise_error
     else
     decrease_balance(money)
-    debit = new_debit(money)
-    save_transaction(debit)
+    debit_trans = new_debit(money)
+    save_transaction(debit_trans)
 end
   end
+
 
   def print_statement
     @statement.print(transaction_history)
--- lib/statement.rb
+++ lib/statement.rb
@@ -7,14 +7,14 @@ class Statement
   end
 
   def format(number)
-    number == 0 ? '' : '%.2f' % number
+    number == 0 || number == nil ? '' : '%.2f' % number
   end
 
   def print(my_transactions)
     puts header
-    array = my_transactions.map do | transaction|
-       "#{transaction.date} ||  #{transaction.credit}   || "\
-       "#{transaction.debit}   ||    #{transaction.balance}\n"
+    array = my_transactions.map do |transaction|
+       "#{transaction.date} ||  #{format(transaction.credit)}   || "\
+       "#{format(transaction.debit)}   ||    #{format(transaction.balance)}\n"
     end
     puts array.reverse.join('')
     end
