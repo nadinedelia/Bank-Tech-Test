a5dbdad58b149a7796b13ee10cfd42d55a2e1814
nadinedelia <nadine.delia@yahoo.com>
2020-07-15 22:09:36 +0100

    refactor

--- spec/statement_spec.rb
+++ spec/statement_spec.rb
@@ -8,8 +8,9 @@ describe Statement do
   let(:transaction) { double(date: '15/07/2020', credit: '42.00', debit: '00.00', balance: '42.00') }
 
   it 'displays correct formatting' do
-    formatting = "date || credit || debit || balance\n\n"\
+    expect { statement.print(transaction_history) }.to output(
+      "date || credit || debit || balance\n\n"\
     "15/07/2020 || 42.00 || 0.00 || 42.00\n"
-    expect { statement.print(transaction_history) }.to output(formatting).to_stdout
+    ).to_stdout
   end
 end
