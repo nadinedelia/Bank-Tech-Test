1c2d33810f134450c9a053c893eee7d4c66d78a9
nadinedelia <nadine.delia@yahoo.com>
2020-07-15 14:49:14 +0100

    test for transaction passing

--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -1,18 +1,23 @@
-# frozen_string_literal: true
-
 require 'transaction'
 
-# date || credit || debit || balance
-# 14/01/2012 || || 500.00 || 2500.00
-# 13/01/2012 || 2000.00 || || 3000.00
-# 10/01/2012 || 1000.00 || || 1000.00
-
 describe Transaction do
-  xit 'is initialised with a date, credit, debit and balance' do
-    transaction = Transaction.new(date, credit, debit, balance)
-    expect(transaction.date).to eq(Time.now.strftime('%d/%m/%Y'))
-    expect(transaction.credit).to eq(1000)
-    expect(transaction.debit).to eq(0)
-    expect(transaction.balance).to eq(1000)
-  end
+
+    it "makes a deposit" do
+      transaction = Transaction.new(credit: 100, balance: 100)
+      expect(transaction.date).to eq(Time.now.strftime("%d/%m/%Y"))
+      expect(transaction.credit).to eq(100)
+      expect(transaction.debit).to eq(nil)
+      expect(transaction.balance).to eq(100)
+    end
+  
+
+    it "makes a withdrawal" do
+      transaction = Transaction.new(debit: 200, balance: 400)
+      expect(transaction.date).to eq(Time.now.strftime("%d/%m/%Y"))
+      expect(transaction.credit).to eq(nil)
+      expect(transaction.debit).to eq(200)
+      expect(transaction.balance).to eq(400)
+    end
+
+
 end
