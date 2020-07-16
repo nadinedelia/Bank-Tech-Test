9a641a918ccd269a98cc4fadeebfc29883303bc7
nadinedelia <nadine.delia@yahoo.com>
2020-07-16 12:31:11 +0100

    lint;

--- lib/account.rb
+++ lib/account.rb
@@ -7,7 +7,6 @@ require_relative 'formatting'
 class Account
   attr_reader :balance, :transaction_history
 
-
   def initialize(transaction = Transaction, statement = Statement.new)
     @balance = 0
     @date = Time.now.strftime('%d/%m/%Y')
@@ -18,6 +17,7 @@ class Account
 
   def deposit(money)
     raise "It's not possible to deposit a negative amount" if money < 0
+
     @balance += money
     transaction = @transaction.new(credit: money, balance: @balance)
     save_transaction(transaction)
@@ -25,13 +25,13 @@ class Account
 end
 
   def withdraw(money)
-    raise "Transaction not possible - insufficient funds" if @balance < money || money < 0
+    raise 'Transaction not possible - insufficient funds' if @balance < money || money < 0
+
     @balance -= money
     transaction = @transaction.new(debit: money, balance: @balance)
     save_transaction(transaction)
     decrease_message(money)
   end
-
 
   def print_statement
     @statement.print(transaction_history)
